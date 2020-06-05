c90d39d7f8faa791093677f68f2251df591eb4eb
unknown <macjay314@gmail.com>
2020-06-05 10:01:03 -0700

    Create ViewModels, add missing using statements

---
+++ PierresTreats/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+namespace PierresTreats.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
